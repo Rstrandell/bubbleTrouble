5bacd21eb30e09016a93fdddc4ca3a28877ed542
Rstrandell <razmus@student.chalmers.se>
2019-11-25 14:03:24 +0100

    added missing scripts and prefabs

---
+++ Assets/NewGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGame : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject gameOverScreen;
+    public List<GameObject> activeBalls;
+    public bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        ball = Instantiate(ball);
+        ball.transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(250, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver)
+        {
+            for (int i = 0; i < activeBalls.Count; i++)
+            {
+                GameObject tmp = activeBalls[i];
+                tmp.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                tmp.GetComponent<Rigidbody2D>().angularVelocity = 0;
+                tmp.GetComponent<Rigidbody2D>().isKinematic = true;
+            }
+            Instantiate(gameOverScreen);
+            GameObject.Find("Player").GetComponent<Player>().transform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GameObject.Find("Player").GetComponent<Player>().transform.GetComponent<Rigidbody2D>().angularVelocity = 0;
+        }
+        
+    }
+}
---
+++ Assets/ProjectileScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    private NewGame manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.Find("Manager").GetComponent<NewGame>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!manager.gameOver){
+            transform.Translate(new Vector3(0, 10 * Time.deltaTime, 0));
+        }
+       
+
+    }
+
+    void spawnNewBalls(Collider2D collision)
+    {
+        int direction = 1;
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject temp = Instantiate(collision.gameObject);
+            temp.transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(direction * 150, 250));
+            Vector2 size = temp.GetComponent<Renderer>().bounds.size;
+
+            Vector2 rescale = temp.transform.localScale;
+
+            rescale.y = 0.5f * rescale.y;
+            rescale.x = 0.5f * rescale.x;
+
+            temp.transform.localScale = rescale;
+            temp.tag = "ball";
+            direction *= -1;
+            manager.activeBalls.Add(temp);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ball")
+        {
+            if (collision.gameObject.transform.localScale.x >= 0.07f)
+            {
+                spawnNewBalls(collision);
+            }
+            manager.activeBalls.Remove(collision.gameObject);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
