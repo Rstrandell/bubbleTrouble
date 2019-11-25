3c01ca7fce782e5141ff1d8f2770b07c3085842a
Rstrandell <razmus@student.chalmers.se>
2019-11-25 13:12:17 +0100

    added ball functionality

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public GameObject[] projectile;
 
     public enum Direction { LEFT, RIGHT, UP, DOWN };
     // Start is called before the first frame update
@@ -22,6 +23,12 @@ public class Player : MonoBehaviour
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0));
             Debug.Log("hej");
 
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject temp = Instantiate(projectile[0], transform.position, Quaternion.identity);
+            temp.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 500));
         }
 
     }
